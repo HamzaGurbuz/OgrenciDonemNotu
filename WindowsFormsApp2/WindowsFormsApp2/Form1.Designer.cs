9758a9e4eb8e3403206801ef107d63da17f4128d
Necmettin Hamza Gürbüz <139889728+Necmettin-Hamza-Gurbuz@users.noreply.github.com>
2024-05-05 22:25:34 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstOgrenciler = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(235, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 253);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Veri Girişi";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ortalama :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Öğrenci Adı :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.lstOgrenciler);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(602, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 401);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlem";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(17, 338);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 48);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Takdir Listesi";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 275);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 48);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Teşekkür Listesi";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 48);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Geçen Listesi";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Kalan Listesi";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lstOgrenciler
+            // 
+            this.lstOgrenciler.FormattingEnabled = true;
+            this.lstOgrenciler.Location = new System.Drawing.Point(154, 31);
+            this.lstOgrenciler.Name = "lstOgrenciler";
+            this.lstOgrenciler.Size = new System.Drawing.Size(230, 355);
+            this.lstOgrenciler.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tümünü Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(1193, 649);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstOgrenciler;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int say = 0;
+        int ort = 0;
+        int tum;
+        int[] kalan = new int[5];
+        int[] gecen = new int[5];
+        int[] tesekkur = new int[5];
+        int[] takdir = new int[5];
+
+        string[] ogr = new string[5];
+
+        int[] ortalama = new int[5];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (say < ogr.Length)
+            {
+                ogr[say] = textBox1.Text;
+                ortalama[say] = int.Parse(textBox2.Text);
+
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+
+                if (ortalama[say] < 50)
+                {
+                    kalan[say] = ortalama[say];
+                }
+                else if (ortalama[say] >= 50)
+                {
+                    gecen[say] = ortalama[say];
+                }
+                if (ortalama[say] >= 70)
+                {
+                    if (ortalama[say] < 85)
+                    {
+                        tesekkur[say] = ortalama[say];
+                    }
+                }
+                if (ortalama[say] >= 85)
+                {
+                    takdir[say] = ortalama[say];
+                }
+                say++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lstOgrenciler.Items.Clear();
+            say = 0;
+            for (int i = 0; i < ogr.Length; i++)
+            {
+                lstOgrenciler.Items.Add(ogr[say] + " - " + ortalama[say]);
+                say++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lstOgrenciler.Items.Clear();
+            say = 0;
+            for (int i = 0; i < ogr.Length; i++)
+            {
+                lstOgrenciler.Items.Add(ogr[say] +  " - " + kalan[say]);
+                say++;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lstOgrenciler.Items.Clear();
+            say = 0;
+            for (int i = 0; i < ogr.Length; i++)
+            {
+                lstOgrenciler.Items.Add(ogr[say] + " - " + gecen[say]);
+                say++;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            lstOgrenciler.Items.Clear();
+            say = 0;
+            for (int i = 0; i < ogr.Length; i++)
+            {
+                lstOgrenciler.Items.Add(ogr[say] + " - " + tesekkur[say]);
+                say++;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lstOgrenciler.Items.Clear();
+            say = 0;
+            for (int i = 0; i < ogr.Length; i++)
+            {
+                lstOgrenciler.Items.Add(ogr[say] + " - " + takdir[say]);
+                say++;
+            }
+        }
+    }
+}
